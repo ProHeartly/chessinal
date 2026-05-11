1a7b28084e192ff1d9ee4f0b7693415f7820abd3
HeartlyTheCoderr <proheartlyplayz@gmail.com>
2026-05-11 21:54:13 +0545

    Alpha Release
    Made everything ready for alpha release

---
+++ README.md
@@ -0,0 +1,31 @@
+# Chessinal
+![chessinal](updates_ss\update_5.png)
+
+**Chess in your terminal..** Build with textual library for those beautiful UI and custom script for chess mech. No heavy graphics, just raw logic and TUI :D. For now play with your friends by pass and play! later I will add multiplay (EXAMS ARE GOING ON RN SORRYYY!)
+
+## Nothing much to say..
+## Look at the ss of updates... or kinda devlogs of main UI.
+
+### 1
+![chessinal](updates_ss\update_1.png)
+
+### 2
+![chessinal](updates_ss\update_2.png)
+
+### 3
+![chessinal](updates_ss\update_3.png)
+
+### 4
+![chessinal](updates_ss\update_4.png)
+
+### 5
+![chessinal](updates_ss\update_5.png)
+
+### 6
+![chessinal](updates_ss\update_6.png)
+
+
+## Build command
+```
+pyinstaller --onefile --name Chessinal --collect-all textual --collect-all rich --exclude-module PyQt5 --exclude-module PySide2 --exclude-module PySide6 --exclude-module PyQt6 --exclude-module matplotlib --exclude-module numpy --exclude-module tkinter main.py
+```
--- main.py
+++ main.py
@@ -12,9 +12,108 @@ from screens.board_ui import BoardScreen
 # Version -> Alpha 1.6: Added special cases like Pawn promotion, en passant and castling
 # Version -> Alpha 1.7: Added check and pinning.
 # Version -> Alpha 1.8: Added game over, changed UI and made it look better! Also added checkmating and check check iykyk
+# Version -> Alpha Release: Made everything ready for alpha release
 
 class ChessApp(App):
-    CSS_PATH = "style.tcss"
+    CSS = """
+Horizontal {
+    height: 100%;
+}
+
+#sidebar {
+    width: 30;
+    background: $panel;
+    dock: right;
+    border-left: tall $accent;
+    padding: 1;
+}
+
+#history-list {
+    height: 1fr;
+    border: solid $accent;
+}
+
+#sidebar-title {
+    background: $primary;
+    color: white;
+    padding: 1;
+    margin-bottom: 1;
+    text-align: center;
+}
+
+ListItem {
+    padding: 0 1;
+}
+
+#board-grid {
+    layout: grid;
+    grid-size: 8 8;
+    grid-columns: 1fr;
+    grid-rows: 1fr;
+    width: 100;
+    height: 50;
+    align: center middle;
+    border: round gold;
+    padding: 0;
+    margin: 1;
+}
+
+ChessPiece {
+    width: 1fr;
+    height: 1fr;
+    content-align: center middle;
+    border: none;
+    min-width: 12;
+    min-height: 6;
+}
+
+ChessPiece > Label {
+    width: 100%;
+    height: 100%;
+    content-align: center middle;
+}
+
+ChessPiece:focus {
+    text-style: none;
+    border: round gold;
+}
+
+.light:hover, .light:focus {
+    background: #ebecd0;
+}
+
+.dark:hover, .dark:focus {
+    background: #779556;
+}
+
+.light {
+    background: #ebecd0;
+    color: #fdfdfd;
+}
+
+.dark {
+    background: #779556;
+    color: #fdfdfd;
+}
+
+.selected {
+    text-style: italic;
+}
+
+.piece {
+    text-style: bold;
+}
+
+#dialog {
+    width: 50;
+    height: 18;
+    background: $panel;
+    border: thick gold;
+    padding: 1 2;
+    content-align: center middle;
+    margin-top: 10;
+}
+"""
     
     def on_mount(self) -> None:
         self.board = Board()
